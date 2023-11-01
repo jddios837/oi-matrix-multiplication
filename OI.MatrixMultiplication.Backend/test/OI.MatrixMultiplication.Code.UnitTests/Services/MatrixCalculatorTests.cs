2ee1ae7370b67f7059f2d815b9fc29b440a26ab6
jddios837 <jddios837@gmail.com>
2023-11-01 17:35:05 -0500

    Adding basic unit tests

---
+++ OI.MatrixMultiplication.Backend/test/OI.MatrixMultiplication.Code.UnitTests/Services/MatrixCalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿using OI.MatrixMultiplication.Core.Services;
+
+namespace OI.MatrixMultiplication.Code.UnitTests.Services;
+
+public class MatrixCalculatorTests
+{
+    [Fact]
+    public void MatrixMultiplication_ValidMatrices_ReturnsResult()
+    {
+        // Arrange
+        var matrixCalculator = new MatrixCalculator();
+        int[,] matrixA = new int[,] { { 1, 2 }, { 3, 4 } };
+        int[,] matrixB = new int[,] { { 5, 6 }, { 7, 8 } };
+
+        // Act
+        int[,] result = matrixCalculator.MultiplyMatrices(matrixA, matrixB);
+
+        // Assert
+        int[,] expectedResult = new int[,] { { 19, 22 }, { 43, 50 } };
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Fact]
+    public void MatrixMultiplication_InvalidMatrices_ThrowsException()
+    {
+        // Arrange
+        var matrixCalculator = new MatrixCalculator();
+        int[,] matrixA = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+        int[,] matrixB = new int[,] { { 7, 8 }, { 9, 10 } }; // Invalid dimensions for multiplication
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => matrixCalculator.MultiplyMatrices(matrixA, matrixB));
+    }
+}
---
+++ OI.MatrixMultiplication.Backend/test/OI.MatrixMultiplication.Code.UnitTests/Validations/MatrixRequestValidatorTests.cs
@@ -0,0 +1,65 @@
+﻿using FluentValidation.TestHelper;
+using OI.MatrixMultiplication.Core.Models;
+using OI.MatrixMultiplication.Core.Validations;
+
+namespace OI.MatrixMultiplication.Code.UnitTests.Validations;
+
+public class MatrixRequestValidatorTests
+{
+    [Fact]
+    public void ValidMatrixRequest_ShouldNotHaveValidationErrors()
+    {
+        // Arrange
+        var validator = new MatrixRequestValidator();
+        var request = new MatrixRequest
+        {
+            MatrixA = new int[][] { new int[] { 1, 2 }, new int[] { 3, 4 } },
+            MatrixB = new int[][] { new int[] { 5, 6 }, new int[] { 7, 8 } }
+        };
+
+        // Act
+        var result = validator.TestValidate(request);
+
+        // Assert
+        result.ShouldNotHaveValidationErrorFor(x => x.MatrixA);
+        result.ShouldNotHaveValidationErrorFor(x => x.MatrixB);
+    }
+
+    [Fact]
+    public void InvalidMatrixRequest_ShouldHaveValidationErrors()
+    {
+        // Arrange
+        var validator = new MatrixRequestValidator();
+        var request = new MatrixRequest
+        {
+            MatrixA = new int[][]
+            {
+                new int[] { 1, 2, 3 }, 
+                new int[] { 4, 5, 6 },
+                new int[] { 4, 5, 6 },
+                new int[] { 4, 5, 6 },
+                new int[] { 4, 5, 6 },
+                new int[] { 4, 5, 6 },
+                new int[] { 4, 5, 6 },
+            },
+            MatrixB = new int[][]
+            {
+                new int[] { 7, 8 }, 
+                new int[] { 9, 10 }, 
+                new int[] { 11, 12 },
+                new int[] { 11, 12 },
+                new int[] { 11, 12 },
+                new int[] { 11, 12 },
+                new int[] { 11, 12 },
+            }
+        };
+
+        // Act
+        var result = validator.TestValidate(request);
+
+        // Assert
+        Assert.NotEmpty(result.Errors);
+        Assert.Contains(result.Errors, error => error.PropertyName == "MatrixA" && error.ErrorMessage == "MatrixA must have between 1 and 5 rows and columns.");
+        Assert.Contains(result.Errors, error => error.PropertyName == "MatrixB" && error.ErrorMessage == "MatrixB must have between 1 and 5 rows and columns.");
+    }
+}
